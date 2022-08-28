0dc3aaae3e86a893d21ef2dd322d71464194d9f1
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-08-28 23:05:21 +0630

    add env

--- Porfolio/settings.py
+++ Porfolio/settings.py
@@ -12,15 +12,18 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3i*cmcun%$jd#k4)75*ees*!wgs%7l7fbnuax5#l76%$&33w&b'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
