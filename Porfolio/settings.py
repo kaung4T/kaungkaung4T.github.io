fc445fe3fa30e145d8bc9c3f5c4f757ea7e7fc3b
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-07-15 12:06:27 +0630

    update

--- Porfolio/settings.py
+++ Porfolio/settings.py
@@ -143,8 +143,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = config('MAIL_HOST')
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mrkaungminnkhant@gmail.com'
-EMAIL_HOST_PASSWORD = 'ifpfkhtdsfxeobug'
+EMAIL_PORT = config('MAIL_PORT')
+EMAIL_HOST_USER = config('MAIL_USER')
+EMAIL_HOST_PASSWORD = config('MAIL_PASSWORD')
